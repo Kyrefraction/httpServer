56a8443a878c1894081aff534693d4d9d64916c8
Kyrefraction <v.scialpi@hotmail.co.uk>
2017-03-10 11:37:57 +0000

    Update EmployeeDAO.java

--- EmployeeDAO.java
+++ EmployeeDAO.java
@@ -2,27 +2,6 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class EmployeeDAO {
-	private Connection c = null;
-	private Statement s = null;
-	private ResultSet r = null;
-	
-	public void getConnection() throws SQLException, ClassNotFoundException {
-		String dbURL = "jdbc:sqlite:empdb:sqlite";
-		c = DriverManager.getConnection(dbURL);
-		s = c.createStatement();
-		Class.forName("org.sqlite.JDBC");
-	}
-	public void closeConnection() throws SQLException {
-		if (r != null) {
-			r.close();
-		}
-		if (s != null) {
-			s.close();
-		}
-		if (c != null) {
-			c.close();
-		}
-	}
 
 	public static ArrayList<Employee> selectAllEmployees()
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
@@ -37,84 +16,6 @@ public class EmployeeDAO {
 			con = DriverManager.getConnection(dbURL);
 			st = con.createStatement();
 			rs = st.executeQuery("SELECT * FROM employees;");
-			while (rs.next()) {
-				Employee E = new Employee(  rs.getString("ID"), rs.getString("Name"), rs.getString("Gender"),
-						rs.getString("DOB"), rs.getString("Address"), rs.getString("Postcode"), rs.getString("NIN"),
-						rs.getString("jobTitle"), rs.getString("StartDate"), rs.getString("Salary"),
-						rs.getString("Email"));
-				employeeList.add(E);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}  finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (st != null) {
-					st.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-		}
-		return employeeList;
-	}
-
-	public ArrayList<Employee> selectEmployeeByName(String name) throws ClassNotFoundException, SQLException {
-		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
-		Class.forName("org.sqlite.JDBC");
-		ArrayList<Employee> employeeList = new ArrayList<Employee>();
-		try {
-
-			String dbURL = "jdbc:sqlite:empdb.sqlite";
-			con = DriverManager.getConnection(dbURL);
-			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM employees WHERE Name LIKE '" + name + "';");
-			while (rs.next()) {
-				Employee E = new Employee(  rs.getString("ID"), rs.getString("Name"), rs.getString("Gender"),
-						rs.getString("DOB"), rs.getString("Address"), rs.getString("Postcode"), rs.getString("NIN"),
-						rs.getString("jobTitle"), rs.getString("StartDate"), rs.getString("Salary"),
-						rs.getString("Email"));
-				employeeList.add(E);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}  finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (st != null) {
-					st.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-		}
-		return employeeList;
-	}
-	
-	public ArrayList<Employee> selectEmployeeByID(int currentID) throws ClassNotFoundException, SQLException {
-		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
-		Class.forName("org.sqlite.JDBC");
-		ArrayList<Employee> employeeList = new ArrayList<Employee>();
-		try {
-
-			String dbURL = "jdbc:sqlite:empdb.sqlite";
-			con = DriverManager.getConnection(dbURL);
-			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM employees WHERE ID LIKE '" + currentID + "';");
 			while (rs.next()) {
 				Employee E = new Employee(  rs.getString("ID"), rs.getString("Name"), rs.getString("Gender"),
 						rs.getString("DOB"), rs.getString("Address"), rs.getString("Postcode"), rs.getString("NIN"),
@@ -172,33 +73,6 @@ public class EmployeeDAO {
 			}
 		}
 		System.out.println("Employee successfully created");
-	}
-
-	public void insertEmployeeAtId(Employee employee, int identified)
-			throws ClassNotFoundException, SQLException {
-		Connection connection = null;
-		Statement statement = null;
-		String breaker = "', '";
-		try {
-			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection("jdbc:sqlite:empdb.sqlite");
-			connection.setAutoCommit(false);
-			statement = connection.createStatement();
-			String sql = "INSERT INTO employees (ID, Name, Gender, DOB, Address, Postcode, NIN, jobTitle, StartDate, Salary, Email)"
-					+ " VALUES (" + identified + breaker + employee.getName() + breaker + employee.getGender() + breaker
-					+ employee.getDob() + breaker + employee.getAddress() + breaker + employee.getPostcode() + breaker
-					+ employee.getNatInscNo() + breaker + employee.getTitle() + breaker + employee.getStartDate() + breaker
-					+ employee.getSalary() + breaker + employee.getEmail() + "');";
-			statement.executeUpdate(sql);
-			connection.commit();
-			statement.close();
-			connection.close();
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(0);
-		}
-		System.out.println("Delete operation successfully done");
-
 	}
 
 	public static void deleteEmployeeById(int identified) {
